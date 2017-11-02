92e8d551354120edfe76fc258992b1025bfed05c
RUNQI SHAO <shaorunqi@gmail.com>
2017-11-01 17:07:29 -0700

    change some components

--- src/components/Team.js
+++ src/components/Team.js
@@ -4,8 +4,8 @@ import { Jumbotron } from 'reactstrap';
 const Team = (props) => {
   return (
     <div id = "Team">
-      <Jumbotron style = {{"marginBottom":"0px", "background-color": "black", "margin-top": "-40px"}}>
-        <h1 className="display-3" style = {{"color": "white"}}>Meet the <span style = {{"font-weight": "800"}}>team!</span></h1>
+      <Jumbotron style = {{"marginBottom":"0px", "backgroundColor": "black", "marginTop": "-40px"}}>
+        <h1 className="display-3" style = {{"color": "white"}}>Meet the <span style = {{"fontWeight": "800"}}>team LS -Z!</span></h1>
         <p className="lead" style = {{"color": "white"}}>We are a team of professionals</p>
       </Jumbotron>
     </div>
